5b0ea1b962de4f174daa43a1efc686cb0675c277
AjayRahul1 <p.ajayrahul02@gmail.com>
2023-11-25 22:50:54 +0530

    main.py added
    Functions:
    1. Startup event calls setup function in setup_fns.py
    2. Root Home page
    3. Prompt Response that can be called in index.html

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
+import os
+import setup_fns
+
+db=0
+chatHistory={}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  global db
+  db = setup_fns.setup()
+  yield
+  os.remove('chatHistory.json')
+
+app = FastAPI(lifespan=lifespan)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+  global chatHistory
+  chatHistory = {}
+  return templates.TemplateResponse("index.html",{"request": request})
+
+@app.get('/getPromptResponse')
+def getPromptResponse(request: Request):
+  global chatHistory
+  query=request.query_params['chatPrompt']
+  chatHistory = setup_fns.inputQueryResponse(query, db, chatHistory)
+  return templates.TemplateResponse("shortTemplates/chatHistory.html", {"request": request, "chatHistory": chatHistory})
